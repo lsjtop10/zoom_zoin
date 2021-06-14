d6e48f6369272f165ae5d2d126ee04f2e5c4346f
lsjtop10 <lsjtop10@gmail.com>
2021-06-14 18:20:35 +0900

    파일 추가,

--- zoom_zoin/main.c
+++ zoom_zoin/main.c
@@ -1,54 +1,20 @@
 #include <stdlib.h>
 #include "timetable.h"
 
-#ifdef _DEBUG
-void test()
+TimeTable* timeTable;
+
+void LoadTimeTableFromFile();
+void initlizeProgram(); //초반 작업
+
+void LoadTimeTableFromFile()
 {
-	TimeTable* tt = initializeTimeTable();
-	DateTime dt;
-	dt.tm_hour = 1;
-	dt.tm_min = 0;
-	Class lic = { "0","",  dt};
-	Class math = { "1","", dt};
-	Class eng = { "2","",  dt};
-	Class phy = { "3", "", dt};
-	Class lic1 = { "4","",  dt };
-	Class math2 = { "5","", dt };
-	Class eng3 = { "6","",  dt };
-	Class phy4 = { "7", "", dt };
-	enqueue(tt, lic);
-	enqueue(tt, math);
-	enqueue(tt, eng);
-	enqueue(tt, phy);
-	enqueue(tt, lic1);
-	enqueue(tt, math2);
-	enqueue(tt, eng3);
-	enqueue(tt, phy4);
-
-	getNodeByIndex(tt, 3);
-
-	swapTimeTable(tt, 0, 3);
-	swapTimeTable(tt, 0, 3);
-
-	swapTimeTable(tt, 4, 7);
-	swapTimeTable(tt, 4, 7);
-
-	swapTimeTable(tt, 2, 3);
-	swapTimeTable(tt, 2, 3);
-
-	swapTimeTable(tt, 4, 5);
-	swapTimeTable(tt, 4, 5);
+	timeTable = initializeTimeTable();
+	
+	//todo: 여기다가 시간표 load해서 enqueue하는 부분 구현
 }
-
-#endif
-
 
 int main()
 {
-	//test코드로 점프
-#ifdef _DEBUG
-	test();
-#endif
-
+	initlizeProgram();
 
 }
--- zoom_zoin/settings.h
+++ zoom_zoin/settings.h
@@ -1,2 +1,7 @@
 #pragma once;
 
+typedef struct Settings
+{
+	int UsedProgram;
+	int wait_min;
+} Settings;
