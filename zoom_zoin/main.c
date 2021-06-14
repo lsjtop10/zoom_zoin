d187595b7596d23237413095278788812d747851
lsjtop10 <lsjtop10@gmail.com>
2021-06-14 18:50:48 +0900

    병합

--- zoom_zoin/main.c
+++ zoom_zoin/main.c
@@ -1,29 +1,25 @@
-#include <stdlib.h>
-#include "timetable.h"
 #include "settings.h"
 #include "fun_data.h"
+#include "timetable.h"
+
 
 TimeTable* timeTable;
 
-void LoadTimeTableFromFile();
-void initlizeProgram(); //초반 작업
+//void LoadTimeTableFromFile();
+//void initlizeProgram(); //초반 작업
+//void joinZoom()
 
 void LoadTimeTableFromFile()
 {
 	timeTable = initializeTimeTable();
-	
-	//todo: 여기다가 시간표 load해서 enqueue하는 부분 구현
-	struct Settings setting;
+
+//todo: 여기다가 시간표 load해서 enqueue하는 부분 구현
+
+	Settings setting;
 
 	setting.UsedProgram = Load_Setting(0);
 	setting.wait_min = Load_Setting(1);
 
-
-
-}
-
-void Test()
-{
 
 }
 
@@ -31,4 +27,10 @@ int main()
 {
 	initlizeProgram();
 
+	shwoInitMenu();
+
+	while (true)
+	{
+
+	}
 }
