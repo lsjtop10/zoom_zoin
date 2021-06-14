418c48a21e3cece7f67a76ff13f0c80b713fefc6
fish500m <fishfish500m@naver.com>
2021-06-14 18:47:43 +0900

    6:47 - ..

--- zoom_zoin/main.c
+++ zoom_zoin/main.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include "timetable.h"
+#include "settings.h"
+#include "fun_data.h"
 
 TimeTable* timeTable;
 
@@ -11,6 +13,18 @@ void LoadTimeTableFromFile()
 	timeTable = initializeTimeTable();
 	
 	//todo: 여기다가 시간표 load해서 enqueue하는 부분 구현
+	struct Settings setting;
+
+	setting.UsedProgram = Load_Setting(0);
+	setting.wait_min = Load_Setting(1);
+
+
+
+}
+
+void Test()
+{
+
 }
 
 int main()
