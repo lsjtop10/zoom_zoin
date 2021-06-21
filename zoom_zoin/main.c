ba87c58a67829fdbfa5f3e7ca35faf1952e1e460
lsjtop10 <lsjtop10@gmail.com>
2021-06-21 17:55:41 +0900

    메인로직 구현 얼추 됨

--- zoom_zoin/main.c
+++ zoom_zoin/main.c
@@ -96,19 +96,61 @@ void initalizeProgram()
 
 }
 
-void joinZoom(Class target)
+void joinZoom(Class *target)
 {
 
 	//실행 스크립트를 저장하는 데 필요한 메모리를 동적 할당
-	char* cmd = (char*)malloc(sizeof(char) * (strlen("start ") + strlen(getProgramByIndex(getSettings()->UsedProgram)) + strlen(target.zoomAdd)) + 5);
+	char* cmd = (char*)malloc(sizeof(char) * (strlen("start ") + strlen(getProgramByIndex(getSettings()->UsedProgram)) + strlen(target->zoomAdd)) + 5);
 	strcpy(cmd, "start ");
 	strcat(cmd, getProgramByIndex(getSettings()->UsedProgram));
 	strcat(cmd, " \"");
-	strcat(cmd, target.zoomAdd);
+	strcat(cmd, target->zoomAdd);
 	strcat(cmd, "\"");
 	system(cmd);
 }
-	
+
+int convertDTtoMin(DateTime input)
+{
+	int min = 0;
+	min += input.tm_hour * 60;
+	min += input.tm_min;
+	return min;
+}
+
+DateTime convertMintoDT(int input)
+{
+	DateTime dt;
+	dt.tm_hour = input / 60;
+	dt.tm_min = input % 60;
+	return dt;
+}
+
+
+int compareTime(DateTime input0, DateTime input1)
+{
+	int min0 = convertDTtoMin(input0);
+	int min1 = convertDTtoMin(input1);
+
+	if (min0 >= min1)
+	{
+		return 0;
+	}
+	else
+	{
+		return 1;
+	}
+}
+
+//
+DateTime subtractTime(DateTime input1, DateTime input2)
+{
+	int min1 = convertDTtoMin(input1);
+	int min2 = convertDTtoMin(input2);
+
+	min2 = min1 - min2;
+
+	return convertMintoDT(min2);
+}
 
 int main()
 {
@@ -118,5 +160,22 @@ int main()
 	while (true)
 	{
 		//여기에 loop 처리
+
+		//head불러오기
+		Class* head = lookHead(timeTable);
+		//현재 시간 불러오기
+		time_t timer;
+		DateTime *current;
+		timer = time(NULL); // 현재 시각을 초 단위로 얻기
+
+		current = localtime(&timer); 
+		//만약 현재 시간이 줌 시작시간 분단위 - 사전 대기시간이면
+		if (compareTime(subtractTime(head->startTime, convertMintoDT(getSettings()->wait_min)), *current) == 1)
+		{
+			joinZoom(head);
+			cutHead(timeTable);
+		}
+		//줌 접속
+		//머리 자르기
 	}
 }
